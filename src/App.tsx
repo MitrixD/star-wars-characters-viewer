77d66bcd3f26ba406ecbd74e15d5d8a6acd12e3c
Mitrix <kremencov.r2d2@gmail.com>
2023-05-20 16:47:57 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, TextField } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="contained" color="primary">
+        Material-UI Button
+      </Button>
+      <TextField label="Material-UI TextField" variant="outlined" />
+      <p className="text-red-500">Tailwind CSS Text</p>
     </div>
   );
 }
